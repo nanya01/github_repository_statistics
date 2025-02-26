83be286b06033cc3f4c2339f203195d6b822f040
nanya01 <peterahunanya79@gmail.com>
2025-02-26 19:40:52 +0400

    completed_total_forks_for_user

--- main.py
+++ main.py
@@ -10,12 +10,23 @@ def get_user_repos(username):
     })
 
     if response.status_code == 200:
-        print(response.json())
-        print(f"Length {len(response.json())}")
+        # print(f"Length {len(response.json())}")
+        return response.json()
 
 
-
+def get_total_forks_user_repos(username):
+    #url = f"{BASE_URL}/repos/{username}/{repo}/forks"
+    total_forks = 0
+    repos = get_user_repos(username)
+    print(f"repos {repos}")
+    for repo in repos:
+        total_forks += repo["forks_count"]
+        #print(f"repo {repo["forks_count"]}")
+        
+    #print(f"total_forks {total_forks}")
+    
 
 
 if __name__=='__main__':
    get_user_repos("kaggle")
+   get_total_forks_user_repos("kaggle")
